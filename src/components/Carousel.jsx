efd4e1680f0f007636c64ed1c0a1280bfd309689
Earljay Juatchon <earljay0606@gmail.com>
2023-09-01 10:21:38 +0800

    ............

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,26 +1,74 @@
   import React, { useState, useEffect } from "react";
-  import { Carousel } from "@material-tailwind/react";
+  import { Carousel, IconButton } from "@material-tailwind/react";
 
   export function CarouselDefault() {
 
    
     return (
       <div className="w-full flex items-center justify-center">
-        <div className="w-3/4">
-          <div className="colorlay">
-            <div class="mx-auto text-center md:max-w-xl lg:max-w-3xl">
-              <h3 class="mb-6 text-3xl font-bold text-primary-800 dark:text-primary-500">
-                Testimonials
-              </h3>
-              <p class="mb-6 pb-2 md:mb-12 md:pb-0">
-                Customer satisfaction is one of our mission. Take a look at our
-                customer reviews to us for your references.
-              </p>
-            </div>
-
-            <Carousel>
-              <div
-                class="gap-6 text-center md:cols-3 lg:gap-12 md:px-10 ">
+        <div className="w-full md:w-3/4">
+          <div class="mx-auto text-center md:max-w-xl lg:max-w-3xl">
+            <h3 class="mb-6 text-3xl font-bold text-primary-800 dark:text-primary-500">
+              Testimonials
+            </h3>
+            <p class="mb-6 pb-2 md:mb-12 md:pb-0">
+              Customer satisfaction is one of our mission. Take a look at our
+              customer reviews to us for your references.
+            </p>
+          </div>
+          <div className="bg-blue-300/10 backdrop-blur-sm drop-shadow-lg shadow-md p-6 rounded-lg">
+            <Carousel
+              className="rounded-xl"
+              prevArrow={({ handlePrev }) => (
+                <IconButton
+                  variant="text"
+                  color="black"
+                  size="lg"
+                  onClick={handlePrev}
+                  className="!absolute top-3/4 left-0 -translate-y-2/4"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                    />
+                  </svg>
+                </IconButton>
+              )}
+              nextArrow={({ handleNext }) => (
+                <IconButton
+                  variant="text"
+                  color="black"
+                  size="lg"
+                  onClick={handleNext}
+                  className="!absolute top-3/4 !right-0 -translate-y-2/4"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </IconButton>
+              )}
+            >
+              <div class="gap-6 text-center md:cols-3 lg:gap-12 md:px-10 ">
                 <div class="mb-12 md:mb-0">
                   <div class="mb-6 flex justify-center">
                     <img
@@ -299,7 +347,7 @@
                     </li>
                   </ul>
                 </div>
-              </div> 
+              </div>
             </Carousel>
           </div>
         </div>
